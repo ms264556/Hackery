45088979a9c5bdcb40b5f4b2c0892a107b748272
Tony <ms264556@users.noreply.github.com>
2022-09-05 05:06:44 +0100

    Update create_zd_licenses_patch.sh

--- Scripts/create_zd_licenses_patch.sh
+++ Scripts/create_zd_licenses_patch.sh
@@ -98,7 +98,7 @@ echo "Added AP Licenses.\n<br />"
 END
 
 chmod +x upgrade_check.sh
-rm -f zd.patch.tar zd.patch.tar.gz
+rm -f zd.patch.tgz
 tar czf zd.patch.tgz metadata all_files upgrade_check.sh
 rks_encrypt zd.patch.tgz zd.licenses.patch.img
 rm all_files metadata upgrade_check.sh zd.patch.tgz
